4f947859a276f3cfe02655cc9014e1c3085f817e
cuongtieu-kim <cuongtk@tdc.edu.vn>
2019-03-13 08:27:37 +0700

    Test

--- app/src/main/java/vn/edu/tdc/manhinhtuongtac/TouchScreenActivity.java
+++ app/src/main/java/vn/edu/tdc/manhinhtuongtac/TouchScreenActivity.java
@@ -9,5 +9,6 @@ public class TouchScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.touch_screen_layout);
+
     }
 }
