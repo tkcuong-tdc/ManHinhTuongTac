8d3dcb7141c21b05320065d8a87b6388e5cd6713
Cuong Tieu-Kim <cuongtieu-kim@Cuongs-Air.lan>
2019-03-13 08:15:07 +0700

    Khởi tạo Project cho dự án Chuyên đề di động 2019, bởi Cương Tiêu Kim

---
+++ app/src/main/java/vn/edu/tdc/manhinhtuongtac/TouchScreenActivity.java
@@ -0,0 +1,13 @@
+package vn.edu.tdc.manhinhtuongtac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TouchScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.touch_screen_layout);
+    }
+}
